d6388bab1037453ef62b97049abbc02782d3a62a
LeeAFH <footehud@gmail.com>
2017-10-15 22:57:01 -0400

    setup basic grid and cols for app

--- reactui/src/App.js
+++ reactui/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-// import Compbuttons from "./components/Compbuttons";
 import CodeEditor from "./components/editor";
 // import AceEditor from "./components/editor";
 import FissionButton from "./components/fissionButton";
 import PopoverExampleAnimation from "./components/sidebarmenu";
 import ButtonExample from "./components/compbuttons";
+import Grid from "react-bootstrap/lib/Grid";
+import Col from "react-bootstrap/lib/Col";
+import Row from "react-bootstrap/lib/Row";
 
 class App extends Component {
   render() {
@@ -15,20 +16,30 @@ class App extends Component {
       <div className="App">
          <nav>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo">FISSION</a>
+            <a href="www.google.com" className="brand-logo">FISSION</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><a href="collapsible.html">Sign-in</a></li>
             </ul>
             </div>
         </nav>
-        <FissionButton />
-        <div id="infobox">This is info on how to use the app</div>
-             
+        
+        <div id="infobox"><FissionButton />This is info on how to use the app</div>
+  
+           <Grid>
+          <Row> 
+          <Col  xs={12} md={8}>
         <CodeEditor />
+        </Col>
+        <Col xs={6} md={4}> <PopoverExampleAnimation />
+        </Col>
+        </Row>
+        </Grid>
+
+
          <ButtonExample />
-       <PopoverExampleAnimation />
-      
+       
                   </div>
+                                   
     );
   }
 }
